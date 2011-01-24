f798823ed9b6ce53781c9d2f2dd27197d09e4c58
bournenapster <bournenapste@gmail.com>
2011-01-24 19:12:48 +0530

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Perk::Application.config.secret_token = 'db53d63f2d8c3042bf7843f5a835de7d7feb3499e2db625c56b5dbd985aeca6697717472ca366817d1288db28de5113f36154a64d39cb998e890460b0a697622'
